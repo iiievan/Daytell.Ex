b8d5024f5ee2b2128289e4d724eeadfe59bb1417
i.shopov <shopov@author.spb.ru>
2018-06-26 18:52:01 +0300

    Выполнил упражнение со страницы 465, ну как выполнил напечатал просто, в упражнении вся соль в инициализации const переменной на этапе инициализации класса.

--- Daytell.exd/Daytell/main.cpp
+++ Daytell.exd/Daytell/main.cpp
@@ -1,13 +1,48 @@
-#include "class_time.h"
+//#include "class_time.h"
+#include <iostream>
+#include <string> 
 
+using namespace std;
+
+class Increment
+{
+public:
+    Increment(int c = 0, int i = 1);
+    void addIncrement() { count += increment; }
+    void print() const;
+
+private:
+    int count;
+    const int increment;    // константный элемент данных
+};
+
+// Конструктор класса Increment
+Increment::Increment(int c, int i)
+    : increment(i)
+{ count = c;}
+
+void Increment::print() const
+{
+    cout << "count = " << count
+         << ", increment = " << increment << endl;
+}
 
 int main()
 {
-	const Time t(19, 33, 52);   // константный объект
+    setlocale(LC_ALL, "Russian");
 
-    t.set_hour(20);
-    t.set_minute(12);
-    t.set_second(39);    
+    Increment value(10, 5);
+
+    cout << "Перед приращением: ";
+
+    value.print();
+
+    for (int j = 1; j <= 3; j++)
+    {
+        value.addIncrement();
+        cout << "После приращения " << j << ": ";
+        value.print();
+    }
 
 	return 0; // exit the application
 }
